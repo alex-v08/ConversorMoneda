7356d2d8fc80c25216444e081655342099ff313f
alex08-vt <alex.velasquez08@outlook.com>
2022-10-06 13:47:34 -0300

    Archivos subidos

--- ConversorMoneda/src/alura/com/Conversorpanel.java
+++ ConversorMoneda/src/alura/com/Conversorpanel.java
@@ -1,12 +1,21 @@
 package alura.com;
 
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 
 public class Conversorpanel {
 
     private List <Paises> paises;
     private List <Monedas> Monedas;
+
+    private ArrayList <String> listaPaises;
+
+
+    
+
 
 
 
